78cce7ad27db7f0e4978cfc95a3bafad1590277b
Pallav2011 <ranpisepg09@gmail.com>
2023-12-17 18:44:58 +0530

    new app is created
    new app is created and some functionality is added.

---
+++ API/-- SQLite.sql
@@ -0,0 +1,9 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"Pandurang");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"Dattaraj");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"Vishal");
