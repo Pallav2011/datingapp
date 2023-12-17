dbfec246e5b6217d7b9cbe4c18eae7f9425d8a7b
Pallav2011 <ranpisepg09@gmail.com>
2023-12-17 23:46:28 +0530

    nav component is created and account service is created
    nav component is created and account service is created

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +13,25 @@ export class AppComponent implements OnInit{
   title = 'Dating app';
   users : any; 
 
-  constructor(private htpp: HttpClient){}
+  constructor(private htpp: HttpClient, private accountService: AccountService){}
   ngOnInit(): void {
+  this.getUser();
+  this.setCurrentUser();
+  }
+
+  getUser(){
     this.htpp.get('https://localhost:5001/api/users').subscribe({
       next: responce => this.users = responce,
       error: error => console.log(error),
       complete : () => console.log('Request has completed')      
     })
   }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
+  }
+
 }
